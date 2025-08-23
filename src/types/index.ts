7af29e433f305088eba331326908d2ea167b863d
to-tsunomori <to-tsunomori@kddi-agile.com>
2025-08-23 08:01:38 +0000

    feat: V6Clientのインポートを削除し、コードを整理

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,3 @@
-import { GraphQLOptionsV6, V6Client } from "@aws-amplify/api-graphql";
+import { GraphQLOptionsV6 } from "@aws-amplify/api-graphql";
 
 export type AuthMode = NonNullable<GraphQLOptionsV6["authMode"]>;
