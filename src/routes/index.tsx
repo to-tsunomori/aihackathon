2866245437ba1e0bd46ee583c5a8830fb0cec85b
to-tsunomori <to-tsunomori@kddi-agile.com>
2025-08-23 00:32:50 +0000

    feat: BreakpointDebuggerおよびBreakpointSettingsコンポーネントを削除

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ResearchHub } from "../component/ResearchHub";
-import { BreakpointDebugger } from "../component/BreakpointDebugger";
 import { Box } from "@mui/material";
 
 function Index() {
 	return (
 		<Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
 			<ResearchHub />
-			<BreakpointDebugger />
 		</Box>
 	);
 }
